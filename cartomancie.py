b321c89bd33b7a585097ef080fab183dbac9cdfa
AAR2603 <124086097+AAR2603@users.noreply.github.com>
2023-02-01 11:46:59 +0100

    Create cartomancie.py

---
+++ cartomancie.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*-coding:UTF-8 -*
+#
+# Anne Abitbol
+
+from random import randint
+
+cards_list = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valet', 'Dame', 'Roi', 'As']
+colors_list = ['Carreau', 'Coeur', 'Pique', 'Trèfle']
+
+print("Carte tirée au hasard : " + cards_list[randint(0, 12)] + " de " + colors_list[randint(0, 3)])
